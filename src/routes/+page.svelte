042e0fe2c22076c1e2f4ec33154d125640796b6c
Martin Plessy <plessy.martin@outlook.com>
2024-06-30 17:58:22 +0100

    Add sveltestrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
-	import { browser } from "$app/environment"
-	import { onMount } from "svelte"
-	import "../app.scss"
-	import { GlobeEuropeAfrica } from "svelte-bootstrap-icons"
+  import { Container, Icon, Input, Styles } from "@sveltestrap/sveltestrap"
 
-	onMount(async () => {
-		if (!browser) return
-		const bootstrap = await import("bootstrap")
-	})
-
-	let theme = false
-
-	$: {
-		if (browser) {
-			document.documentElement.setAttribute("data-bs-theme", theme ? "light" : "dark")
-		}
-	}
+	let darkTheme = true
 </script>
 
-<input type="checkbox" bind:checked={theme}/>
-<div class="container py-4 px-3 mx-auto">
-	<h1>Hello, <GlobeEuropeAfrica/>!</h1>
-	<button class="btn btn-primary">Primary button</button>
-</div>
+<Styles theme={darkTheme ? "dark" : "light"} />
+
+<Container>
+	<h1>Coucou, <Icon name="globe-europe-africa" />!</h1>
+	<Input type="switch" label="Dark theme" bind:checked={darkTheme} />
+</Container>
