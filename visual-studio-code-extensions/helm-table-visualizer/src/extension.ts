8d7a62337fe5f31d50861fca2d0bba26b01c27a0
Martin Plessy <plessy.martin@outlook.com>
2024-07-09 14:31:17 +0100

    Cat coding 2

--- visual-studio-code-extensions/helm-table-visualizer/src/extension.ts
+++ visual-studio-code-extensions/helm-table-visualizer/src/extension.ts
@@ -1,16 +1,17 @@
 import * as vscode from "vscode"
 
 export function activate(context: vscode.ExtensionContext) {
-  const catUris = {
-    coding: vscode.Uri.joinPath(context.extensionUri, "assets", "cats", "coding.gif"),
-    compiling: vscode.Uri.joinPath(context.extensionUri, "assets", "cats", "compiling.gif"),
-    testing: vscode.Uri.joinPath(context.extensionUri, "assets", "cats", "testing.gif"),
-  }
+  let previewForChartPanel: vscode.WebviewPanel | null = null
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("helmTableVisualizer.commands.openPreviewForChart", () => {
-      const previewForChartPanel = vscode.window.createWebviewPanel(
-        "helmTableVisualizer.webViews.previewForChart",
+    vscode.commands.registerCommand("helmTableVisualizer.commands.openPreview", () => {
+      if (previewForChartPanel) {
+        previewForChartPanel.reveal()
+        return
+      }
+
+      previewForChartPanel = vscode.window.createWebviewPanel(
+        "helmTableVisualizer.webViews.preview",
         "Helm Table Visualizer",
         {
           viewColumn: vscode.ViewColumn.Active
@@ -18,42 +19,93 @@ export function activate(context: vscode.ExtensionContext) {
         {
           localResourceRoots: [
             vscode.Uri.joinPath(context.extensionUri, "assets")
-          ]
+          ],
+          enableScripts: true
         }
       )
 
-      let currentCatUri: vscode.Uri = null!
+      previewForChartPanel.webview.html = /* html */ `
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8"/>
+            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${ previewForChartPanel.webview.cspSource}; script-src 'unsafe-inline' ${previewForChartPanel.webview.cspSource}; style-src 'unsafe-inline' ${previewForChartPanel.webview.cspSource };"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>${ previewForChartPanel.title }</title>
+          </head>
+          <body>
+            <h1 id="codeCount"></h1>
+            <img id="catImg" src="" width="300"/>
+            <script>
+              ;(() => {
+                const vscode = acquireVsCodeApi()
 
-      function tick() {
-        currentCatUri =
-          currentCatUri == catUris.coding ? catUris.compiling :
-          currentCatUri == catUris.compiling ? catUris.testing :
-          catUris.coding
+                const codeCount = document.getElementById("codeCount")
+                const catImg = document.getElementById("catImg")
 
-        previewForChartPanel.webview.html = /*html*/`
-          <!DOCTYPE html>
-          <html lang="en">
-            <head>
-              <meta charset="UTF-8"/>
-              <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-              <title>${ previewForChartPanel.title }</title>
-            </head>
-            <body>
-              <h1>Wow Such Cat</h1>
-              <img src="${ previewForChartPanel.webview.asWebviewUri(currentCatUri) }" width="300"/>
-            </body>
-          </html>
-        `
-      }
+                const catUris = {
+                  coding: "${ previewForChartPanel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "assets", "cats", "coding.gif"))}",
+                  compiling: "${previewForChartPanel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "assets", "cats", "compiling.gif"))}",
+                  testing: "${previewForChartPanel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "assets", "cats", "testing.gif")) }",
+                }
 
-      tick()
-      const interval = setInterval(tick, 1000)
+                let i = 0
+
+                function code() {
+                  ++i
+                  codeCount.textContent = i.toString()
+                }
+
+                let currentCatUri = null
+
+                function tick() {
+                  currentCatUri =
+                    currentCatUri == catUris.coding ? catUris.compiling :
+                    currentCatUri == catUris.compiling ? catUris.testing :
+                    catUris.coding
+                  catImg.src = currentCatUri
+
+                  vscode.postMessage("WOW")
+                }
+
+                code()
+                setInterval(code, 200)
+
+                tick()
+                setInterval(tick, 1000)
+
+                window.addEventListener("message", e => {
+                  if (e.data == "REFACTOR") {
+                    i = Math.ceil(i / 2)
+                  }
+                }, false)
+              })();
+            </script>
+          </body>
+        </html>
+      `
+
+      context.subscriptions.push(
+        previewForChartPanel.webview.onDidReceiveMessage(m => {
+          if (m == "WOW") {
+            vscode.window.showInformationMessage("Wow...")
+          }
+        })
+      )
 
       context.subscriptions.push(
         previewForChartPanel.onDidDispose(() => {
-          clearInterval(interval)
         })
       )
+    })
+  )
+  context.subscriptions.push(
+    vscode.commands.registerCommand("helmTableVisualizer.commands.openLockedPreview", () => {
+      if (!previewForChartPanel) {
+        return
+      }
+
+      previewForChartPanel.webview.postMessage("REFACTOR")
     })
   )
 }
