44251de6ae8761d77a48da0ab004601247924364
Martin Plessy <plessy.martin@outlook.com>
2024-07-09 17:46:54 +0100

    Yaml

--- visual-studio-code-extensions/helm-table-visualizer/assets/webViews/preview/main.js
+++ visual-studio-code-extensions/helm-table-visualizer/assets/webViews/preview/main.js
@@ -1,3 +1,5 @@
+main()
+
 /**
  * @param {object} vscode
  * @param {() => any} vscode.getState
@@ -5,9 +7,43 @@
  * @param {(newState: any) => void} vscode.setState
  */
 function main(vscode = acquireVsCodeApi()) {
-  window.addEventListener("message", e => {
-    vscode.postMessage(e.data)
+  updateData(vscode.getState() ?? {
+    file: "---",
+    yaml: {}
+  })
+
+  window.addEventListener("message", ({ data }) => {
+    vscode.setState(data)
+    updateData(data)
   }, false)
 }
 
-main()
+function updateData(data) {
+  document.body.innerHTML = /* html */ `
+    <table style="width: 100%;">
+      <thead style="text-align: left;">
+        <tr>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${ Object.entries(data.yaml).map(([k, v]) => /* html */ `
+          <tr>
+            <td>${ k }</td>
+            <td>${
+              v === undefined       ? /* html */ `undefined` :
+              v === null            ? /* html */ `null` :
+              typeof v == "number"  ? /* html */ `<input type="number" value="${ v }"/>` :
+              typeof v == "string"  ? /* html */ `<textarea>${ v }</textarea>` :
+              typeof v == "boolean" ? /* html */ `<input type="checkbox" ${ v ? "checked" : ""}/>` :
+              Array.isArray(v)      ? /* html */ `list` :
+              v instanceof Object   ? /* html */ `dict` :
+              ""
+            }</td>
+          </tr>
+        `).join("") }
+      </tbody>
+    </table>
+  `
+}
--- visual-studio-code-extensions/helm-table-visualizer/src/webViews/preview.ts
+++ visual-studio-code-extensions/helm-table-visualizer/src/webViews/preview.ts
@@ -1,42 +1,69 @@
 import * as vscode from "vscode"
+import * as yaml from "js-yaml"
 
 export const id = "helmTableVisualizer.webViews.preview"
 
 export function createWebviewPanel(context: vscode.ExtensionContext): vscode.Disposable {
-  const previewPanel = vscode.window.createWebviewPanel(
-    id,
-    "Helm Table Visualizer",
-    {
-      viewColumn: vscode.ViewColumn.Active
-    },
-    {
-      localResourceRoots: [
-        vscode.Uri.joinPath(context.extensionUri, "assets")
-      ],
-      enableScripts: true
-    }
-  )
+  const panel = vscode.window.createWebviewPanel(id, "Helm Table Visualizer", {
+    viewColumn: vscode.ViewColumn.Active
+  }, {
+    localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, "assets")],
+    enableScripts: true
+  })
 
-  previewPanel.webview.html = /* html */ `
+  panel.webview.html = /* html */ `
     <!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8"/>
-        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${previewPanel.webview.cspSource}; script-src 'unsafe-inline' ${previewPanel.webview.cspSource}; style-src 'unsafe-inline' ${previewPanel.webview.cspSource};"/>
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${panel.webview.cspSource}; script-src 'unsafe-inline' ${panel.webview.cspSource}; style-src 'unsafe-inline' ${panel.webview.cspSource};"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>${previewPanel.title}</title>
-        <script defer src="${previewPanel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "assets", "webViews", "preview", "main.js"))}"></script>
+        <title>${panel.title}</title>
+        <script defer src="${panel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "assets", "webViews", "preview", "main.js"))}"></script>
       </head>
       <body>
       </body>
     </html>
   `
 
+  const activeTextEditor = vscode.window.activeTextEditor
+  if (activeTextEditor) {
+    const activeEditorWorkspacePath = vscode.workspace.getWorkspaceFolder(activeTextEditor.document.uri)?.uri.path ?? ""
+
+    const filePath = activeTextEditor.document.uri.path.startsWith(activeEditorWorkspacePath)
+      ? activeTextEditor.document.uri.path.substring(activeEditorWorkspacePath.length)
+      : activeTextEditor.document.uri.path
+
+    panel.webview.postMessage({
+      file: filePath,
+      yaml: yaml.load(activeTextEditor.document.getText(), { filename: filePath })
+    })
+
+    context.subscriptions.push(
+      vscode.workspace.onDidChangeTextDocument(e => {
+        if (e.document != activeTextEditor.document) {
+          return
+        }
+
+        panel.webview.postMessage({
+          file: filePath,
+          yaml: yaml.load(activeTextEditor.document.getText(), { filename: filePath })
+        })
+      })
+    )
+  }
+
   context.subscriptions.push(
-    previewPanel.onDidDispose(() => {
+    panel.webview.onDidReceiveMessage(m => {
       // ...
     })
   )
 
-  return previewPanel
+  context.subscriptions.push(
+    panel.onDidDispose(() => {
+      // ...
+    })
+  )
+
+  return panel
 }
