8adf66d3dbd3ba2d87614c6dbe56bc56807963a0
Martin Plessy <plessy.martin@outlook.com>
2024-07-13 16:09:16 +0100

    WebView

--- visual-studio-code-extensions/vssvelte/src/extension.ts
+++ visual-studio-code-extensions/vssvelte/src/extension.ts
@@ -1,26 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("todo--give-a-name-to-the-extension.helloWorld", () => {
+      const panel = vscode.window.createWebviewPanel("wv-64073227", "Helm Table Visualizer", {
+        viewColumn: vscode.ViewColumn.Active
+      }, {
+        enableScripts: true
+      })
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "todo--give-a-name-to-the-extension" is now active!');
+      panel.webview.html = /* html */ `
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          </head>
+          <body>
+            <h1>Hibou</h1>
+          </body>
+        </html>
+      `
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('todo--give-a-name-to-the-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TODO: Give a name to the extension!');
-	});
-
-	context.subscriptions.push(disposable);
+      context.subscriptions.push(
+        panel
+      )
+    })
+  )
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+}
