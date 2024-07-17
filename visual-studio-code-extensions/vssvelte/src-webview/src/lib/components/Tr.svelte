dde4f3da8917aac59f388a309b6f2fa311627345
Martin Plessy <plessy.martin@outlook.com>
2024-07-17 14:15:35 +0100

    Toggle keys

--- visual-studio-code-extensions/vssvelte/src-webview/src/lib/components/Tr.svelte
+++ visual-studio-code-extensions/vssvelte/src-webview/src/lib/components/Tr.svelte
@@ -16,9 +16,12 @@
 
 <tr>
   <td style="position: sticky; left: 0; background: var(--vscode-editorStickyScroll-background); border-bottom: 1px solid var(--vscode-tree-tableColumnsBorder); border-right: 1px solid var(--vscode-editorStickyScroll-shadow); padding-left: {depth}em; vertical-align: top;">
-    <code style="white-space: preserve; font-family: var(--vscode-editor-font-family); font-size: var(--vscode-editor-font-size); font-weight: var(--vscode-editor-font-weight); background: none;">
-      { leaf }
-    </code>
+    <label>
+      <input type="checkbox" bind:checked={key.expanded}/>
+      <code style="white-space: preserve; font-family: var(--vscode-editor-font-family); font-size: var(--vscode-editor-font-size); font-weight: var(--vscode-editor-font-weight); background: none;">
+        { leaf }
+      </code>
+    </label>
   </td>
   {#each valuesFiles as v}
     {#if v.toggled}
@@ -28,6 +31,7 @@
     {/if}
   {/each}
 </tr>
+
 {#if key.expanded}
   {#each Object.values(key.children).sort((a, b) => a.order - b.order) as childKey}
     <svelte:self key={childKey} {valuesFiles} />
